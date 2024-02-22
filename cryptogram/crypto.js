8de9743feaf8ebedfb02976a1c97b3212dc0283f
Bill McCutchan <bmccutchanjr@gmail.com>
2024-02-22 01:18:45 -0500

    event handlers for the <textarea>

--- cryptogram/crypto.js
+++ cryptogram/crypto.js
@@ -1,4 +1,44 @@
 window.addEventListener ("load", event =>
 {
-		
+	const input = document.getElementById ("input-quote");
+	input.addEventListener ("blur", event => quoteBlurHandler (event));
+	input.addEventListener ("input", event => quoteInputHandler (event));
 })
+
+function quoteBlurHandler (event)
+{	event.preventDefault();
+	target = event.target;
+
+	//	I thought there were other things to do here, but they seem to work better in the input event handler.  So all I
+	//	really wat to do here, is make sure the 'Encode' button has focus...and even that shouldn't be necessary.  If the
+	//	input event handler works correctly, the 'Encode' button should receive focus naturally.
+
+	document.getElementById ("encode").focus()
+}
+
+function quoteInputHandler (event)
+{	event.preventDefault();
+	target = event.target;
+
+	//	If there is data in the <textarea>, I want to remove the instructions (a background-image) and display the 'Encode'
+	//	button.  But the reverse if there is no data -- display the instructions and hide the 'Encode' button.
+
+	if (target.value == "")
+	{
+		target.classList.add ("instructions");
+		hideElement ("encode");
+	}
+	else
+	{
+		target.classList.remove ("instructions");
+		hideElement ("encode", false);
+	}
+}
+
+function hideElement (element, hide=true)
+{
+	if (hide)
+		document.getElementById (element).classList.add ("hidden");
+	else
+		document.getElementById (element).classList.remove ("hidden");
+}
